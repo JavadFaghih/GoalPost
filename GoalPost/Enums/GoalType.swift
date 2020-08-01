12090f11d17b3eac1b7e9cdae811b74122455fd2
Javad Faghih <javad.4145@gmail.com>
2020-08-01 15:56:27 +0430

    the app was Completed

---
+++ GoalPost/Enums/GoalType.swift
@@ -0,0 +1,13 @@
+//
+//  GoalType.swift
+//  GoalPost
+//
+//  Created by javad faghih on 5/4/1399 AP.
+//
+
+import Foundation
+
+enum GoalType: String {
+    case longTerm = "Long Term"
+    case shortTerm = "shortTerm"
+}
---
+++ GoalPost/Extention/UIButtonExt.swift
@@ -0,0 +1,25 @@
+//
+//  UIButtonExt.swift
+//  GoalPost
+//
+//  Created by javad faghih on 5/6/1399 AP.
+//
+
+import UIKit
+
+extension UIButton {
+    
+    func setSelectedColor() {
+        self.backgroundColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
+        
+    }
+    
+    func setDeselectedColor() {
+        
+        self.backgroundColor = #colorLiteral(red: 0.4922404289, green: 0.7722371817, blue: 0.4631441236, alpha: 1)
+        
+        
+    }
+    
+    
+}
